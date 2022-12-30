657eb7e9f59b50cb0d99679241a195d65faadd12
Liannet Yisel Rodriguez Santiesteban <l.rodriguez@programisto.fr>
2022-12-30 18:09:21 -0300

    env variables and public folder

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+require('dotenv').config();
 
 //Crear el servidor express
 const app = express();
 
+//Directorio publico
+app.use(express.static('public'))
+
 //Rutas
-app.get('/', (req, res) => {
-  res.json({
-    "ok": true
-  })
-})
+// app.get('/', (req, res) => {
+//   res.json({
+//     "ok": true
+//   })
+// })
 
 //Escuchar peticiones
 app.listen(4000, () => {
